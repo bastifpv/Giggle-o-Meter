7bcc91e5df25db3fbbc3e530aaf2147f2e183bf7
Bastian Reuther <bastian.reuther@allod.com>
2024-01-26 23:48:43 +0100

    Add quit function on Exit Button

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,3 +5,9 @@ func _ready():
 
 func _on_play_button_pressed():
 	SceneSwitcher.change_scene("res://scenes/team_turn.tscn", {"active_team":0})
+
+
+
+func _on_exit_button_pressed():
+	SceneSwitcher.quit_game()
+	pass # Replace with function body.
