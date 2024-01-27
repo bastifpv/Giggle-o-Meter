8633dcf6cc254e8ca5f3103663cc5cf69289497e
Max Herchet <49324298+peakpwr@users.noreply.github.com>
2024-01-27 02:14:45 +0100

    added result values to scene

--- scripts/results.gd
+++ scripts/results.gd
@@ -1,7 +1,37 @@
 extends Node2D
 
 func _ready():
-	pass
+	var results_TeamA = GlobalSettings.list_TeamA
+	var cardIDs_TeamA = []
+	var userInput_TeamA = []
+	var score_TeamA = []
+	
+	var results_TeamB = GlobalSettings.list_TeamB
+	var cardIDs_TeamB = []
+	var userInput_TeamB = []
+	var score_TeamB = []
+	
+	
+	for i in results_TeamA:
+		cardIDs_TeamA.append(i["cardID"])
+		userInput_TeamA.append(i["userInput"])
+		score_TeamA.append(i["score"])
+		
+	
+		
+	for j in results_TeamB:
+		cardIDs_TeamB.append(j["cardID"])
+		userInput_TeamB.append(j["userInput"])
+		score_TeamB.append(j["score"])
+		
+		
+	$CanvasLayer/CardsALabel.text = str(cardIDs_TeamA)
+	$CanvasLayer/userInputALabel.text = str(userInput_TeamA)
+	$CanvasLayer/scoreALabel.text = str(score_TeamA)
+		
+	$CanvasLayer/CardsBLabel.text = str(cardIDs_TeamB)
+	$CanvasLayer/userInputBLabel.text = str(userInput_TeamB)
+	$CanvasLayer/scoreBLabel.text = str(score_TeamB)
 
 func _on_next_button_pressed():
 	print("Game finished")
