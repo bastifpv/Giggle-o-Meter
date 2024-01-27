9bedab6a1ccb6dead7069500a50169cc134fcb8f
Bastian Reuther <bastian.reuther@allod.com>
2024-01-27 13:13:10 +0100

    Basic audio analyses to points is working

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
+var array = []
+
 func _ready():
+	
 	print(GlobalSettings.list_TeamA)
 	print(GlobalSettings.list_TeamB)
 	print(GlobalSettings.current_round)
 	$CanvasLayer/TeamBTextureRect.visible = false
 	$CanvasLayer/TeamATextureRect.visible = true
 	_show_team_a()
+	_play_sound()
+	await get_tree().create_timer(3.0).timeout
+	_audio_to_score()
 	
 	await get_tree().create_timer(2.0).timeout
 
@@ -18,6 +24,11 @@ func _ready():
 	#await get_tree().create_timer(2.0).timeout
 	
 	#_on_next_button_pressed()
+
+func _process(delta):
+	if (AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Record"),0) > -180):
+			array.append(AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Record"),0))
+	
 
 func _on_next_button_pressed():
 	GlobalSettings.current_round +=1
@@ -53,8 +64,28 @@ func _calculate_score(list_Team):
 func _record_sound():
 	pass
 	
-func _analyse_sound():
-	pass
+	
+func _play_sound():
+	$ExampleAUdio.play()
+	print("audioDOne")
+	
+func _audio_to_score():
+	var score = 0
+	var scoreArr = []
+	var in_min = -200
+	var in_max = 0
+	var out_max = 100
+	var out_min = 0
+	var counter = 0
+	for x in array:
+		var calcedValue = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+		scoreArr.append(calcedValue)
+		score += calcedValue
+		counter +=1
+	var final_score = score / counter
+	print(final_score)
+	return final_score
+	
 	
 func _paly_text():
 	pass
