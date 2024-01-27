5be437d355adca1279049473bb969038eaffe82c
Max <max.herchet@web.de>
2024-01-27 01:47:21 +0000

    Revert "Added the battle flow logic, Team A and Team B with waiting timer."
    This reverts commit 3133d83e694a162ef278c6e0c61c8772861cdaa0

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -1,20 +1,9 @@
 extends Node2D
 
 func _ready():
-	$CanvasLayer/TeamBTextureRect.visible = false
+	
+	pass
 
-	_show_team_a()
-	
-	await get_tree().create_timer(2.0).timeout
-
-	$CanvasLayer/TeamATextureRect.visible = false
-	$CanvasLayer/TeamBTextureRect.visible = true
-	
-	_show_team_b()
-	
-	await get_tree().create_timer(2.0).timeout
-	
-	_on_next_button_pressed()
 
 func _on_next_button_pressed():
 	GlobalSettings.current_round +=1
@@ -22,28 +11,3 @@ func _on_next_button_pressed():
 		SceneSwitcher.change_scene("res://scenes/results.tscn")
 	else:
 		SceneSwitcher.change_scene("res://scenes/team_turn.tscn")
-
-
-func _show_team_a():
-	
-	var list_TeamA = GlobalSettings.list_TeamA
-	for dictA in list_TeamA:
-		var CardID = dictA["cardID"]
-		var UserInput = dictA["userInput"]
-		$CanvasLayer/TeamATextureRect/SpeechBubbleTextureRect/Label.text = UserInput
-		_calculate_score(list_TeamA)
-		
-func _show_team_b():
-	
-	var list_TeamB = GlobalSettings.list_TeamB
-	for dictB in list_TeamB:
-		var CardID = dictB["cardID"]
-		var UserInput = dictB["userInput"]
-		$CanvasLayer/TeamBTextureRect/SpeechBubbleTextureRect/Label.text = UserInput
-		_calculate_score(list_TeamB)
-		
-func _calculate_score(list_Team):
-	for dictTeam in list_Team:
-		dictTeam["score"] = 6969
-		
-		$CanvasLayer/DecibelOmeterTextureRect/ScoreLabel.text = str(dictTeam["score"])
